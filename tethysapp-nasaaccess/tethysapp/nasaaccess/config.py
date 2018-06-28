199253348dad5b67bbca259e1f243c2c6a4ab3e2
Spencer McDonald <spencermcdonald11@gmail.com>
2018-06-28 16:00:43 +0700

    fix file path

--- tethysapp-nasaaccess/tethysapp/nasaaccess/config.py
+++ tethysapp-nasaaccess/tethysapp/nasaaccess/config.py
@@ -7,4 +7,4 @@ import os
 
 temp_workspace = os.path.join('/home/ubuntu/tethys_temp/nasaaccess')
 
-data_path = os.path.join('/home/ubuntu/nasaaccess_data/')
+data_path = os.path.join('/home/ubuntu/nasaaccess_data')
