117312cb11c9e2b8bf9f7db24414fa7d0dc25fc7
spence97 <spencermcdonald11@gmail.com>
2018-07-19 00:44:52 -0700

    change to adpc file paths

--- tethysapp-nasaaccess/tethysapp/nasaaccess/config.py
+++ tethysapp-nasaaccess/tethysapp/nasaaccess/config.py
@@ -1,7 +1,9 @@
 import os
 
-# data_path = os.path.join('/home/ubuntu/nasaaccess_data/')
+data_path = os.path.join('/home/ubuntu/nasaaccess_data/')
 
-data_path = os.path.join('/home/ubuntu/Documents/nasaaccess_data/')
+temp_path = os.path.join('/home/ubuntu/tethys_temp/nasaaccess')
 
-temp_path = os.path.join('/home/ubuntu/Documents/tethys_temp/nasaaccess')
+# data_path = os.path.join('/home/ubuntu/Documents/nasaaccess_data/')
+#
+# temp_path = os.path.join('/home/ubuntu/Documents/tethys_temp/nasaaccess')
